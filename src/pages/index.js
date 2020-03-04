546a529b424ea3d16ec0c90200e97c4275e57cf1
erdnuss9225 <benjamin@pussel.de>
2020-03-04 15:17:00 +0100

    Teaserseite mit map funktion aufgebaut, create Pages funktion auf gatsby-node.js aufgebaut und slugs automatisch erstellt

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,45 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-export default () => (
-  <Layout>
-    <h1>Startseite mit Artikelteasern</h1>
-  </Layout>
-)
+export default ({data}) => {
+
+  console.log(data)
+
+  return (
+    <Layout>
+      <h1>Startseite mit Artikelteasern</h1>
+      <h4>{data.allMarkdownRemark.totalCount} Artikel verfügbar</h4>
+      {data.allMarkdownRemark.edges.map(({node}) => (
+      
+        <div key={node.id}>
+
+          <img src={node.frontmatter.teaser_image} alt="teaser_image"/>
+          <h3>{node.frontmatter.headline}</h3>
+          <p>{node.frontmatter.abstract}</p>
+
+        </div>
+      
+
+      ) )}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+query {
+  allMarkdownRemark {
+    totalCount
+    edges {
+      node {
+        frontmatter {
+          teaser_image
+          headline
+          slug
+          abstract
+        }
+      }
+    }
+  }
+}
+`
